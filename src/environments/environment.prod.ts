2544c29eb2bedfc220e4cc788fbc843ae1ad03b6
rajasekhar <59217659+Raja-sekhar533@users.noreply.github.com>
2023-01-24 10:54:06 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BACKENDURL: "https://linedisk.com:4000/api",
+  BACKENDURL: "https://linedisk.com:4000/api/",
   NOTIFICATION_URL: "https://linedisk.com:4567/"
 
 
